059a4089cf094a381b0888aa6fd65f97171b5f0e
Nobodysshadow <guenther.wasser@gmail.com>
2018-01-14 19:16:19 +0100

    Version von Daddy
    Version mit mehreren Funktionen

--- Python/guess_number.py
+++ Python/guess_number.py
@@ -1,3 +1,11 @@
+#guess_number.py Application
+"""
+The first small and simple project to have an app that selects a random number between 1 and 99. 
+The player has a maximum of five guesses to find the selected number. 
+By each Guess you will get a feedback if the selected number is bigger or smaller than your guess.
+Hope you find it interessting.
+"""
+
 #Imports and #Import doctype
 """ Import randomize library int """
 from random import randint
